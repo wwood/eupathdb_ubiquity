a1e439294445e537282db1bb699e8c7ec9ab16d6
Ben J Woodcroft <donttrustben near gmail.com>
2008-10-30 15:54:46 +1100

    first version of blast, which does not (yet) work

--- eupathdb_ubiquity.js
+++ eupathdb_ubiquity.js
@@ -45,3 +45,37 @@ CmdUtils.CreateCommand({
     Utils.openUrlInBrowser(urlString);
   }
 })
+/* Currently the BLAST modules do NOT work, because I haven't worked out
+ * how to use POST commands yet */
+CmdUtils.CreateCommand({
+  name: "plasmodb blast protein",
+  takes: {"plasmodb_id": noun_arb_text},
+  homepage: "http://gist.github.com/20872",
+  author: { name: "Ben J. Woodcroft", email: "b.woodcroft@pgrad.unimelb.edu.au"},
+  contributors: ["Ben J. Woodcroft"],
+  license: "GPL3",
+  description: "Blast against Plasmodium falciparum protein sequences using BLASTP",
+  preview: "Blast against Plasmodium falciparum protein sequences using BLASTP",
+  
+  execute: function(sequence) {
+    var urlString = "http://plasmodb.org/plasmo/showSummary.do?questionFullName=GeneQuestions.GenesBySimilarity&myProp(BlastDatabaseType)=Proteins&myProp(BlastAlgorithm)=blastp&myProp(BlastDatabaseOrganism)=Plasmodium+falciparum&myProp(BlastQuerySequence)="+sequence.text+"&myProp(-e)=0.05&myProp(-v)=50&myProp(-b)=50&myProp(-filter)=yes&altPageSize=1000000";
+    Utils.openUrlInBrowser(urlString);
+  }
+})
+  
+CmdUtils.CreateCommand({
+  name: "plasmodb blast transcript",
+  takes: {"plasmodb_id": noun_arb_text},
+  homepage: "http://gist.github.com/20872",
+  author: { name: "Ben J. Woodcroft", email: "b.woodcroft@pgrad.unimelb.edu.au"},
+  contributors: ["Ben J. Woodcroft"],
+  license: "GPL3",
+  description: "Blast against Plasmodium falciparum transcript sequences using BLASTN",
+  preview: "Blast against Plasmodium falciparum transcript sequences using BLASTN",
+  
+  execute: function(sequence) {
+    var urlString = "http://plasmodb.org/plasmo/showSummary.do?questionFullName=GeneQuestions.GenesBySimilarity&myProp(BlastDatabaseType)=Transcripts&myProp(BlastAlgorithm)=blastn&myProp(BlastDatabaseOrganism)=Plasmodium+falciparum&myProp(BlastQuerySequence)="+sequence.text+"&myProp(-e)=0.05&myProp(-v)=50&myProp(-b)=50&myProp(-filter)=yes&altPageSize=1000000";
+    Utils.openUrlInBrowser(urlString);
+  }
+})
+
